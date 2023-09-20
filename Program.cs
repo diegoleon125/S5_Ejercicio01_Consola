e3b70ed389d89be9ce114cb24b2ec22db537ccdd
user <user@LAPTOP-2DK3J1EI>
2023-09-20 18:20:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese el número 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el número 2: ");
+int num2 = int.Parse(Console.ReadLine());
+int producto = 0;
+
+for (int i = 0; i < num2; i++)
+{
+    producto += num1;
+}
+Console.WriteLine("El producto es " + producto);
